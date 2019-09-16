4d7f8b6d683528101f159aefda72206ef873e52b
chipdelmal <chipdelmal@gmail.com>
2019-09-16 08:59:31 -0700

    Works but needs improvements in clusters detection.

--- main.py
+++ main.py
@@ -3,22 +3,26 @@
 # ############################################################################
 # Useful links
 #   https://medium.com/@andrisgauracs/generating-color-palettes-from-movies-with-python-16503077c025
+#   https://buzzrobot.com/dominant-colors-in-an-image-using-k-means-clustering-3c7af4622036
 # ############################################################################
 
 import cv2
 import glob
 import numpy as np
-# from sklearn.cluster import KMeans
+from sklearn.cluster import KMeans
 from sklearn.cluster import MiniBatchKMeans
 from matplotlib import pyplot as plt
 
 def rescaleColor(colorEightBit):
     return [i / 255 for i in colorEightBit]
 
+def rgb_to_hex(rgb):
+    return '#%02x%02x%02x' % (int(rgb[0]), int(rgb[1]), int(rgb[2]))
+
 ##############################################################################
 # Setup paths and clusters number
 ##############################################################################
-(CLST_NUMBER, MAX_ITER) = (10, 100)
+(CLST_NUMBER, MAX_ITER) = (15, 1000)
 (I_PATH,O_PATH) = ('./in/', './out/')
 
 ##############################################################################
@@ -26,22 +30,19 @@ def rescaleColor(colorEightBit):
 ##############################################################################
 filepaths = sorted(glob.glob(I_PATH + '*.jpg'))
 
-path = filepaths[0]
+path = filepaths[1]
+print(path)
 frame = cv2.imread(path)
 
-# (rows, cols, rgb)  = frame.shape
-# (flattenedFrame, clstdColors) = ([None] * (rows * cols), [])
-# for (i, row) in enumerate(frame):
-#     for (j, col) in enumerate(row):
-#         flattenedFrame.append(list(col))
-# kmeans = MiniBatchKMeans(n_clusters=CLST_NUMBER, max_iter=100).fit(flattenedFrame)
 
 (flatFrame, clusters) = ([], [])
-for row in frame:
-    for col in row:
-        flatFrame.append(list(col))
-kmeans = MiniBatchKMeans(n_clusters=CLST_NUMBER, max_iter=MAX_ITER).fit(flatFrame)
-palette = kmeans.cluster_centers_
+frame = frame.reshape((frame.shape[0] * frame.shape[1], 3))
+kmeans = KMeans(n_clusters = CLST_NUMBER).fit(frame)
+(palette, labels) = (kmeans.cluster_centers_, kmeans.labels_)
 rescale = [rescaleColor(color) for color in palette]
 clusters.append(rescale)
-clusters
+sortedClusters = [sorted(cls) for cls in clusters]
+
+fig, ax = plt.subplots(figsize=(10, 5))
+ax.axis('off')
+plt.imshow(list(map(list, zip(*sortedClusters))))

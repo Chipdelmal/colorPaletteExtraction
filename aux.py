28de3d960a90260ebe6d6e169a041596aab0568b
chipdelmal <chipdelmal@gmail.com>
2019-09-17 08:39:33 -0700

    Cleaning up the code a bit

--- aux.py
+++ aux.py
@@ -1,14 +1,12 @@
 
 import cv2
 import numpy as np
+from PIL import Image
 from sklearn.cluster import KMeans
 from sklearn.cluster import MiniBatchKMeans
 
 
 def reshapeColor(colorEightBit):
-    # Transforming 0/255 BGR to 0/1 RGB
-    # colors = list(colorEightBit)
-    # colors.reverse()
     return [i / 255 for i in colorEightBit]
 
 
@@ -20,10 +18,10 @@ def rgb_to_hex(rgb):
     return '#%02x%02x%02x' % (int(rgb[0]), int(rgb[1]), int(rgb[2]))
 
 
-def calcDominantColors(img, clustersNumber=10, maxIter=1000):
+def calcDominantColors(img, cltsNumb=10, maxIter=1000):
     frame = img.reshape((img.shape[0] * img.shape[1], 3))
     # Cluster the colors for dominance detection
-    kmeans = KMeans(n_clusters=clustersNumber, max_iter=maxIter).fit(frame)
+    kmeans = KMeans(n_clusters=cltsNumb, max_iter=maxIter).fit(frame)
     (palette, labels) = (kmeans.cluster_centers_, kmeans.labels_)
     # Rescale the colors for matplotlib
     colors = [reshapeColor(color) for color in palette]
@@ -37,3 +35,17 @@ def calcHexAndRGBFromPalette(palette):
             sortedPalette
         )
     return (hexColors, rgbColors)
+
+
+def genColorBars(img, heightProp, palette):
+    clstNumber = len(palette)
+    (height, width, depth) = img.shape
+    pltAppend = np.zeros((round(height * heightProp), width, depth))
+    (wBlk, hBlk) = (round(width / clstNumber),round(height * heightProp))
+    for row in range(hBlk):
+        colorIter = -1
+        for col in range(width):
+            if (col % wBlk == 0) and (colorIter < clstNumber - 1):
+                colorIter = colorIter + 1
+            pltAppend[row][col] = palette[colorIter]
+    return pltAppend
--- main.py
+++ main.py
@@ -10,54 +10,41 @@ import cv2
 import glob
 import aux
 import numpy as np
-# from sklearn.cluster import KMeans
-# from sklearn.cluster import MiniBatchKMeans
 from matplotlib import pyplot as plt
 from PIL import Image
 
 ##############################################################################
 # Setup paths and clusters number
 ##############################################################################
-(CLST_NUMBER, MAX_ITER) = (8, 1000)
-(I_PATH,O_PATH) = ('./in/', './out/')
-SELECTION = 1
+(CLST_NUM, MAX_ITER, PLT_HEIGHT) = (8, 1000, .05)
+(I_PATH,O_PATH, FILENAME) = ('./in/', './out/', 'loving.jpg')
 
 ##############################################################################
 # Load images
 ##############################################################################
-filepaths = sorted(glob.glob(I_PATH + '*.jpg'))
-path = filepaths[SELECTION]
-print(path)
+path = I_PATH + FILENAME
+bgr = cv2.imread(path)
+img = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
+(height, width, depth) = img.shape
 
 ##############################################################################
 # Cluster for dominance
 ##############################################################################
-bgr = cv2.imread(path)
-img = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
 (colors, labels) = aux.calcDominantColors(
-        img, clustersNumber=CLST_NUMBER, maxIter=MAX_ITER
+        img, cltsNumb=CLST_NUM, maxIter=MAX_ITER
     )
-sortedPalette = [sorted(cls) for cls in [colors]][0]
-(hexColors, rgbColors) = aux.calcHexAndRGBFromPalette(sortedPalette)
+(hexColors, rgbColors) = aux.calcHexAndRGBFromPalette(colors)
+pltAppend = aux.genColorBars(img, PLT_HEIGHT, colors)
 
-(height, width, depth) = img.shape
-paletteAppend = np.zeros((round(height * .1), width, depth))
-
-(wBlock, hBlock) = (round(width / CLST_NUMBER), round(height * .1))
-for row in range(hBlock):
-    colorIter = -1
-    for col in range(width):
-        if (col % wBlock == 0) and (colorIter < CLST_NUMBER - 1):
-            colorIter = colorIter + 1
-        paletteAppend[row][col] = sortedPalette[colorIter]
-
-Image.fromarray((paletteAppend * 255).astype('uint8'), 'RGB')
+##############################################################################
+# Put the image back together
+##############################################################################
 newImg = np.row_stack((
-    paletteAppend * 255,
+    pltAppend * 255,
     np.full((round(height * .02), width, depth), [255,255,255]),
     img,
     np.full((round(height * .02), width, depth), [255,255,255]),
-    paletteAppend * 255
+    pltAppend * 255
 ))
 imgOut = Image.fromarray(newImg.astype('uint8'), 'RGB')
-imgOut.save("./out/palette.png")
+imgOut.save('./out/' + FILENAME.split('.')[0] + '.png')
